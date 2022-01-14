7448fa24f5635580e3b426391783a3942933062b
Svogg <svoggg123@gmail.com>
2022-01-14 18:47:03 +0300

    Обновлено

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import News, Caregory
+from django.http import Http404
 
 
 def index(request):
@@ -19,3 +20,11 @@ def get_category(request, category_id):
         'category': category
     }
     return render(request, template_name='news/category.html', context=context)
+
+
+def view_news(request, news_id):
+    try:
+        news_item = News.objects.get(pk=news_id)
+    except News.DoesNotExist:
+        raise Http404('No Model matches the given query.')
+    return render(request, 'news/view_news.html', {'news_item': news_item})
