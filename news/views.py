82c6e217c20445eb4b32a5a72eb52e5a2c28baac
Svogg <svoggg123@gmail.com>
2022-01-31 01:09:40 +0300

    Обновлено

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import News, Caregory
+from .forms import NewsForm
 from django.http import Http404
 
 
@@ -28,3 +29,13 @@ def view_news(request, news_id):
     except News.DoesNotExist:
         raise Http404('No Model matches the given query.')
     return render(request, 'news/view_news.html', {'news_item': news_item})
+
+
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
